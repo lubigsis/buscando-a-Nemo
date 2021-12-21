652f7ffc68a86af71443fe474454d9b99fe28240
lubigsis <lucre.baluc@gmail.com>
2021-12-21 19:19:37 -0300

    le apliqué funcionalidad con JS, funciones para obtener distancias de las coordenadas y las pistas

--- functions.js
+++ functions.js
@@ -1,15 +1,33 @@
 let getRandomNumber = size =>{
-    return Math.floor(Math.random)() * size;
+    return Math.floor(Math.random() * size);
 }
 
 
 // e: evento del click del user /  target: la coord. donde está el tesoro
 //aplico teorema de pitágoras para obtener distancia de donde hizo click el user hasta donde está el tesoro
 
-let getDistance = (e, target) => {             
-    let diffX = e.offsetX  - target.x;
-    let diffY = e.offsetY - target.y;
+let getDistance = (e, Nemo) => {             
+    let diffX = e.offsetX  - Nemo.x;
+    let diffY = e.offsetY - Nemo.y;
     
     return Math.sqrt((diffX * diffX) + (diffY * diffY));
 
+}
+
+let pistas = distance =>{
+    if (distance < 30){
+        return "Boiling!";
+    }else if (distance < 40){ 
+        return "Really hot!";
+    }else if(distance < 60){
+        return "Hot";
+    }else if (distance < 100){
+        return "Warm...";
+    }else if (distance < 180){
+        return "Cold";
+    }else if (distance < 360){
+        return "Really cold";
+    }else {
+        return "Freezing!";
+    }
 }
--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 const WIDTH = 533;
 const HEIGHT = 400; 
+
+
+/*genero ubicación aleatoria de Nemo*/
+let Nemo = {
+    x: getRandomNumber(WIDTH),
+    y: getRandomNumber(HEIGHT)
+};
+
+/*para ver dónde hizo click el usuario, es decir, la coordenada del user */
+let elementoMapHTML = document.getElementById('mapJS');
+let distanciaRefHTML = document.getElementById('distanceJS');
+let cantidadClicks = 0;
+
+elementoMapHTML.addEventListener('click', function (e){
+    cantidadClicks++;
+    let distance = getDistance(e, Nemo);
+    let distanciaPista = pistas(distance);
+    distanciaRefHTML.innerHTML = distanciaPista;
+
+    if(distance < 20){
+        alert('Great, you have found Nemo in ' + cantidadClicks + ' clicks!');
+        location.reload();
+    }
+})
